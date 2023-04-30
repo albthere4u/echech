ecdcc1ecfd9a9e802cb5e63634e9dcd6d96c31ad
seokdaegeon <sollscherr@gmail.com>
2023-04-30 10:21:29 +0900

    환경 데이터 조회 기능으로 롤백

--- app.py
+++ app.py
@@ -3,24 +3,23 @@ import csv
 from io import StringIO
 from flask import Flask, render_template, request
 from forms import CSVUploadForm
-from utils import evaluate_environment_data
+from utils import process_csv_file
+
 # 데이터베이스를 사용한다면 models에서 db를 import합니다.
 # from models import db, EnvironmentData
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'  # 실제 사용 시 적절한 시크릿 키로 교체해주세요.
 
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     form = CSVUploadForm()
     if form.validate_on_submit():
         csv_data = form.csvfile.data.read().decode('utf-8')
-        csv_reader = csv.reader(StringIO(csv_data), delimiter=',')
-        next(csv_reader)  # 헤더 스킵
-        data = list(csv_reader)
-        evaluated_data = evaluate_environment_data(data)
-        return render_template('upload.html', form=form, data=evaluated_data)
+        csv_file = StringIO(csv_data)
+        environment_data_list = process_csv_file(csv_file)
+
+        return render_template('upload.html', form=form, data_list=environment_data_list)
     return render_template('upload.html', form=form)
 
 
--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import io
 from models import EnvironmentData
 
 def process_csv_file(csv_file):
-    csv_file = io.StringIO(csv_file.stream.read().decode("UTF8"), newline=None)
     csv_reader = csv.reader(csv_file)
     next(csv_reader)  # 헤더 스킵
 
@@ -25,31 +24,3 @@ def process_csv_file(csv_file):
 
     return environment_data_list
 
-def evaluate_environment_data(data):
-    # 각 row의 평가 범위를 설정하는 딕셔너리
-    evaluation_ranges = {
-        "temperature": {"normal": 10, "warning": 20},
-        "wind_direction": {"normal": 10, "warning": 20},
-        "wind_speed": {"normal": 10, "warning": 20},
-        "rainfall": {"normal": 10, "warning": 20},
-        "pressure": {"normal": 10, "warning": 20},
-        "humidity": {"normal": 10, "warning": 20},
-        "soil_temperature": {"normal": 10, "warning": 20},
-        "soil_moisture": {"normal": 10, "warning": 20},
-        "solar_radiation": {"normal": 10, "warning": 20},
-    }
-
-    results = []
-    for row in data:
-        result_row = []
-        for index, value in enumerate(row[1:], start=1):
-            key = data[0][index]  # 해당 컬럼의 키를 가져옵니다.
-            value = float(value)
-            if value < evaluation_ranges[key]["normal"]:
-                result_row.append("정상 환경")
-            elif value < evaluation_ranges[key]["warning"]:
-                result_row.append("주의 요망")
-            else:
-                result_row.append("즉시 조치")
-        results.append([row[0]] + result_row)
-    return results
